660386706d4bd4cd3cd784b61e9d0ba31fd06516
Rafael T. C. Soares <rafaelcba@gmail.com>
2023-06-20 16:44:35 -0400

    improving bootstrap htpasswd script

--- bootstrap-scripts/enable-htpasswd-users.sh
+++ bootstrap-scripts/enable-htpasswd-users.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 clear
+
+readonly SCRIPT_RELATIVE_DIR_PATH=$(dirname -- "${BASH_SOURCE}")
+#echo " This script is located at: $( dirname -- "${BASH_SOURCE}" ) "
+#echo " This script is located at: $( dirname -- "$(readlink -f "${BASH_SOURCE}")" ) "
+
 oc whoami
 [[ $? -gt 0 ]] && echo "💀 make sure you are logged in your Cluster with an cluster-admin user first! oc login..." && exit 1
 
@@ -9,14 +14,14 @@ echo "creating admin and other 5 regular users..."
 #readonly RANDOM_ADMIN_PWD=$(LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 13 ; echo)
 readonly RANDOM_ADMIN_PWD=openshift
 htpasswd -B -b -c ./htpasswd-users admin $RANDOM_ADMIN_PWD
-htpasswd -B -b -c ./htpasswd-users admin openshift
 htpasswd -B -b ./htpasswd-users user1 openshift
 htpasswd -B -b ./htpasswd-users user2 openshift
 htpasswd -B -b ./htpasswd-users user3 openshift
 htpasswd -B -b ./htpasswd-users user4 openshift
 htpasswd -B -b ./htpasswd-users user5 openshift
 
-oc create secret generic htpasswd-secret --from-file=htpasswd=./htpasswd-users -n openshift-config
+oc delete secret htpasswd-secret --ignore-not-found=true -n openshift-config
+oc create secret generic htpasswd-secret --from-file=htpasswd=${SCRIPT_RELATIVE_DIR_PATH}/htpasswd-users -n openshift-config
 # oc replace -f ./oauth.yaml
 oc apply -f - <<EOF
 apiVersion: config.openshift.io/v1
@@ -35,11 +40,11 @@ EOF
 
 oc adm policy add-cluster-role-to-user cluster-admin admin
 oc adm groups new cluster-admins admin
-rm -f ./htpasswd-users
+rm -f ${SCRIPT_RELATIVE_DIR_PATH}/htpasswd-users
 
 echo
 echo "in a couple of minutes you should be able to login with admin user using [admin/$RANDOM_ADMIN_PWD] credentials!"
 echo
 read -e -p "Do you want to remove the system 'kubeadmin' user? [Y/n]" typed_answer
 typed_answer=${typed_answer:-y}
-[[ $typed_answer == "y" ]] && echo "💀 deleting kubeadmin system user" && oc delete secret kubeadmin -n kube-system
+[[ $typed_answer == "y" ]] && echo "💀 deleting kubeadmin system user... Wait a couple of secs for the cluster oauth be ready, so you can authenticate with the new 'admin' user" && oc delete secret kubeadmin --ignore-not-found=true -n kube-system
